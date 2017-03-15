8f522163f3c84a4187c44106f2349ee657c8711c
Eric Bidelman <ebidel@gmail.com>
2017-03-15 16:39:15 +1100

    Remove x11 crap

--- builder/chromeuser-script.sh
+++ builder/chromeuser-script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# nohup google-chrome \
-#   #--headless
-#   --no-first-run \
-#   --disable-gpu \
-#   --user-data-dir=$TMP_PROFILE_DIR \
-#   --remote-debugging-port=9222 'about:blank' &
-nohup google-chrome --no-first-run --disable-gpu --user-data-dir=$TMP_PROFILE_DIR --remote-debugging-port=9222 'about:blank' &
+nohup google-chrome \
+  --headless \
+  --no-first-run \
+  --disable-gpu \
+  --user-data-dir=$TMP_PROFILE_DIR \
+  --remote-debugging-port=9222 'about:blank' &
---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+deployVersion=$1
+
+if [ -z "$deployVersion" ]
+then
+  echo "App version not specified."
+  echo "Usage: deploy.sh `date +%Y-%m-%d`"
+  exit 0
+fi
+
+readonly APPDIR=$(dirname $BASH_SOURCE)
+
+#echo "\nBuilding app version: $deployVersion\n"
+#gulp
+
+#echo "Deploying frontend version: $deployVersion"
+#gcloud app deploy frontend/app.yaml \
+#    --project lighthouse-ci --version $deployVersion
+#    #--account ericbidelman@google.com
+
+echo "Deploying builder version: $deployVersion"
+gcloud app deploy builder/app.yaml \
+    --project lighthouse-ci --version $deployVersion
