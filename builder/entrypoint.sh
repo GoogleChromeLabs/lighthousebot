5cd7978052f5c9d50e93ee8bf27b8334374f75be
Eric Bidelman <ebidel@gmail.com>
2017-03-16 10:59:35 +1100

    config cleanup

--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#/etc/init.d/dbus start
-#/etc/init.d/xvfb start
-#sleep 1s
+# /etc/init.d/dbus start
+# /etc/init.d/xvfb start
+# sleep 1s
 
-#export DISPLAY=:1
+# export DISPLAY=:1
 TMP_PROFILE_DIR=$(mktemp -d -t lighthouse.XXXXXXXXXX)
 
 su chromeuser /chromeuser-script.sh
