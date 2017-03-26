f08a6abae9cf29b346b5fc10757d691d34f89824
Eric Bidelman <ebidel@gmail.com>
2017-03-26 15:54:42 -0700

    Use yarn

--- builder/server.js
+++ builder/server.js
@@ -2,14 +2,27 @@
 
 const express = require('express');
 const exec = require('child_process').execSync;
+const bodyParser = require('body-parser');
 
 const PORT = 8080;
 
 const app = express();
-app.get('/ci', (req, res) => {
-  const url = req.query.url;
-  const format = req.query.format;
+app.use(bodyParser.json());
+
+// app.get('/ci', (req, res) => {
+//   const url = req.query.url;
+//   const format = req.query.format;
+//   const file = `report.${format}`;
+
+//   exec(`lighthouse --output-path=${file} --output=${format} ${url}`);
+//   res.sendFile(`/${file}`);
+// });
+
+app.post('/ci', (req, res) => {
+  const url = req.body.url;
+  const format = req.body.format;
   const file = `report.${format}`;
+
   exec(`lighthouse --output-path=${file} --output=${format} ${url}`);
   res.sendFile(`/${file}`);
 });
