bad5416639869257b55a46887f35b5ddae3d99dd
Eric Bidelman <ebidel@gmail.com>
2017-04-28 10:09:17 -0700

    Use headless

--- builder/chromeuser-script.sh
+++ builder/chromeuser-script.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# Run full chrome
-nohup google-chrome \
-  --no-first-run \
-  --disable-gpu \
-  --disable-translate \
-  --disable-default-apps \
-  --disable-extensions \
-  --disable-background-networking \
-  --disable-sync \
-  --metrics-recording-only \
-  --safebrowsing-disable-auto-update \
-  --disable-setuid-sandbox \
-  --user-data-dir=$TMP_PROFILE_DIR \
-  --remote-debugging-port=9222 'about:blank' &
-
-# Run headless chrome
+# Run full chrome with a bunch of stuff turned off.
 # nohup google-chrome \
-#   --headless \
+#   --no-first-run \
+#   --disable-gpu \
+#   --disable-translate \
+#   --disable-default-apps \
+#   --disable-extensions \
+#   --disable-background-networking \
+#   --disable-sync \
+#   --metrics-recording-only \
+#   --safebrowsing-disable-auto-update \
+#   --disable-setuid-sandbox \
+#   --user-data-dir=$TMP_PROFILE_DIR \
 #   --remote-debugging-port=9222 'about:blank' &
+
+# Run headless chrome.
+nohup google-chrome \
+  --headless \
+  --disable-gpu \
+  --remote-debugging-port=9222 'about:blank' &
--- builder/entrypoint.sh
+++ builder/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-export DISPLAY=:1
+# Note: Uncomment if you don't want to use Headless Chrome.
+# export DISPLAY=:1
+
 TMP_PROFILE_DIR=$(mktemp -d -t lighthouse.XXXXXXXXXX)
 
-/etc/init.d/dbus start
-/etc/init.d/xvfb start
-sleep 1s
+# Note: Uncomment if you don't want to use Headless Chrome.
+# /etc/init.d/dbus start
+# /etc/init.d/xvfb start
+# sleep 1s
 
 su chromeuser /chromeuser-script.sh
 sleep 3s
