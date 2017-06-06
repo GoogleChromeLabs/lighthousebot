f30b232bbfadf2a0d6dc20ada35a81beff418a67
Justin Ribeiro <justin@justinribeiro.com>
2017-06-06 08:25:14 -0700

    Move to stable channel

--- builder/chromeuser-script.sh
+++ builder/chromeuser-script.sh
@@ -16,7 +16,7 @@
 #   --remote-debugging-port=9222 'about:blank' &
 
 # Run headless chrome.
-nohup google-chrome-beta \
+nohup google-chrome-stable \
   --headless \
   --disable-gpu \
   --remote-debugging-port=9222 'about:blank' &
