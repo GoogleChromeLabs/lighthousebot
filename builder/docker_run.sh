d68219694ed92c86e8b08cf22e52b8e946ff88a9
Eric Bidelman <ebidel@gmail.com>
2017-09-13 19:40:26 -0700

    Name container

--- builder/docker_run.sh
+++ builder/docker_run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -d -p 8080:8080 --cap-add=SYS_ADMIN lighthouse_ci
+docker kill lighthouse_ci
+docker run -d -p 8080:8080 --rm --name lighthouse_ci --cap-add=SYS_ADMIN lighthouse_ci
