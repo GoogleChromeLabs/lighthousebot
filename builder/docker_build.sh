22dc569901cf03fdeb050f4eb00bb6a9c4b3ca9f
Joseph Page (JOPA) <jpage@octo.com>
2018-10-15 11:45:03 +0200

    working Chrome conf behind corporate firewall

--- builder/docker_build.sh
+++ builder/docker_build.sh
@@ -3,4 +3,4 @@
 # Switch if you want to Lighthouse with full Chrome instead of headless.
 # docker build -f Dockerfile.nonheadless -t lighthouse_ci . --build-arg CACHEBUST=$(date +%d)
 
-docker build -t lighthouse_ci . --build-arg CACHEBUST=$(date +%d)
+docker build -t docker.io/josephpage/lighthouse_ci:latest . --build-arg CACHEBUST=$(date +%d)
